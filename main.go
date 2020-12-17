fdf0cd816936fc008fe7a821f536f16aba182f7d
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-12-17 09:55:01 +0000

    Some updates

--- main.go
+++ main.go
@@ -1,77 +1,20 @@
 package main
 
-import ("fmt"; "net/http"; "html/template"; "database/sql"; "github.com/go-sql-driver/mysql")
+import ("fmt"; "net/http"; "html/template")
 
-// User model
-type User struct {
-	Name string `json:"name"`
-	Age uint16 `json:"age"`
-	Money int16
-	AvgGrades, Happiness float64
-	Hobbies []string
-}
-
-func (u User) getAllInfo() string {
-	return fmt.Sprintf("User name is: %s. He is %d and" +
-	 "he has %d dollars", u.Name, u.Age, u.Money)
-}
-
-func (u *User) setNewName(name string) {
-	u.Name = name
-}
-
-func homePage(w http.ResponseWriter, r *http.Request)  {
-	bob := User{"Bob", 24, -50, 4.2, 0.8, []string{"Football", "Dancing"}}
-	bob.setNewName("Max")
-	// fmt.Fprintf(w, bob.getAllInfo())
-	template, _ := template.ParseFiles("templates/home_page.html")
-	template.Execute(w, bob)
-}
-
-func contactsPage(w http.ResponseWriter, r *http.Request)  {
-	fmt.Fprintf(w, "Contacts page")
+func index(w http.ResponseWriter, r *http.Request)  {
+	template, err := template.ParseFiles("templates/index.html", "templates/header.html", "templates/footer.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+	template.ExecuteTemplate(w, "index", nil)
 }
 
 func handleRequest()  {
-	http.HandleFunc("/", homePage)
-	http.HandleFunc("/contacts/", contactsPage)
+	http.HandleFunc("/", index)
 	http.ListenAndServe(":5000", nil)
 }
 
 func main()  {
-	// var bob User = ...
-	// bob := User{name: "Bob", age: 24, money: -50, avgGrades: 4.2, happiness: 0.8}
-
 	handleRequest()
-
-	db, err := sql.Open("mysql", "login:password@tcp(127.0.0.1:3306)/dbname")
-	if err != nil {
-		panic(err)
-	}
-
-	defer db.Close()
-
-	// Add data
-	insert, err := db.Query("INSERT INTO `users` (`name`, `age`) VALUES (`Alex`, 25)")
-	if err != nil {
-		panic(err)
-	}
-	defer insert.Close()
-
-	//Query data
-	res, err := db.Query("SELECT * from `users`")
-	if err != nil {
-		panic(err)
-	}
-
-	for res.Next() {
-		var user User
-		err = res.Scan(&user.Name, &user.Age)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Println(fmt.Sprintf("User: %s with age %d", user.Name, user.Age))
-	}
-
-	fmt.Println("Connected")
 }
