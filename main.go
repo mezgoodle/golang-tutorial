87a75f6fed73062a3195ae5a7e1b7450a5afc585
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-12-25 08:43:14 +0000

    Work with SQL

--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 package main
 
-import ("fmt"; "net/http"; "html/template")
+import ("fmt"; "net/http"; "html/template"; "database/sql"; "github.com/go-sql-driver/mysql")
 
 func index(w http.ResponseWriter, r *http.Request)  {
 	template, err := template.ParseFiles("templates/index.html", "templates/header.html", "templates/footer.html")
@@ -18,10 +18,33 @@ func create(w http.ResponseWriter, r *http.Request)  {
 	template.ExecuteTemplate(w, "create", nil)
 }
 
+func saveArticle(w http.ResponseWriter, r *http.Request)  {
+	title := r.FormValue("title")
+	anons := r.FormValue("anons")
+	fullText := r.FormValue("full_text")
+
+	db, err := sql.Open("mysql", "login:password@tcp(127.0.0.1:3306)/dbname")
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	insert, err := db.Query(fmt.Sprintf("INSERT INTO `articles` (`title`, `anons`, `full_text`) VALUES ('%s', '%s', '%s')", title, anons, fullText))
+	if err != nil {
+		panic(err)
+	}
+	defer insert.Close()
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+
 func handleRequest()  {
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
 	http.HandleFunc("/", index)
 	http.HandleFunc("/create", index)
+	http.HandleFunc("/save_article", saveArticle)
 	http.ListenAndServe(":5000", nil)
 }
 
